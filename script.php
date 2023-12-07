d01055915d83af4f0782d97d960903b1f549542d
Sergey Tolkachyov <musstudent@mail.ru>
2023-12-07 11:22:57 +0400

    - правки языковых констант - правка версии

--- script.php
+++ script.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package       WT Add products info to Joomla script options
- * @version       2.0.0
+ * @version       2.0.1
  * @Author        Sergey Tolkachyov, https://web-tolk.ru
  * @copyright     Copyright (C) 2023 Sergey Tolkachyov
  * @license       GNU/GPL http://www.gnu.org/licenses/gpl-3.0.html
